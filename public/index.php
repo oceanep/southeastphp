8fd6fa91dd1c6ba5b48ced7e37bb601af2f11b14
matt trask <mjftrask@gmail.com>
2015-07-24 18:39:00 -0400

    added css, scripts, twig and other stuff

--- public/index.php
+++ public/index.php
@@ -1,12 +1,27 @@
 <?php
 
-date_default_timezone_set('America/New_York');
 require 'vendor/autoload.php';
+date_default_timezone_set('America/New_York');
+
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.txt', Monolog\Logger::WARNING));
+$log->addWarning('Oh Noes.');
+
 
 $app = new \Slim\Slim();
 
-$app->get('/', function(){
-	echo "hello world";
+$view = $app->view();
+$view->parserOptions = array(
+	'debug' => true;
+);
+
+$view->parserExtension = array(
+	new \Slim\Views\TwigExtension();
+);
+
+$app->get('/', function() use($app) {
+	
 });
 
 $app->run();
